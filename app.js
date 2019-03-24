71929df31fa2fbe11f9e57c1159c55001d1f6b80
Hasan Gharehhasan Loo <hasan@sam-media.com>
2019-03-24 14:13:19 +0800

    Added cron to fetch currencies and insert into database

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const cron = require("node-cron");
+const request = require('request');
+const db = require('./db')
 require('dotenv').config()
 
 const app = express()
 const port = 3000
 
 // schedule tasks to be run on the server
+// to fetch latest exchange rate for all currencies
 cron.schedule(`*/${process.env.INTERVAL} * * * *`, function() {
-    console.log("running a task every minute");
+    console.log("running a task to fetch currencies");
+    request('https://blockchain.info/ticker', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        var currentDateTime = new Date().toLocaleString();
+        Object.keys(body).forEach(function(key) {
+            const q = 'INSERT INTO rates(time, currency, rate) values($1, $2, $3)'
+            const data = [currentDateTime, key, body[key].last]
+            db.query(q, data);
+        });
+    });
+    console.log("running task to fetch currencies finished");
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
